432b1277812e6a6286923196d07861d6aad4dd12
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-11-08 15:19:29 +0300

    update login and register forms

---
+++ public/js/modal.js
@@ -0,0 +1,13 @@
+var modal1 = document.getElementById('modal1');
+window.onclick = function(event) {
+    if (event.target == moda1l) {
+        this.modal1.style.display = "none";
+    }
+}
+
+var modal2 = document.getElementById('modal2');
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        this.modal2.style.display = "none";
+    }
+}

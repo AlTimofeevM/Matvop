05916e93f222bca01c5f47a35a5183bf1f91b9e3
AlTimofeevM <al.timofeev.m@yandex.ru>
2020-04-10 21:28:03 +0300

    db connection close

--- test/db.test.js
+++ test/db.test.js
@@ -1,47 +1,59 @@
 const db = require('../controllers/dbController')
+const mongoose = require('mongoose')
+describe('DB', function() {
 
-it("should find user by id", async () => {
-     
-    let expectedResult = "AlTimofeevM"
-    let result =  await db.findUserById("5e55785f222236202364b390");
-    if(result.token!==expectedResult){
-        throw new Error(`Expected ${expectedResult}, but got ${result}`)
-    }
+    after(function (done) {
+        mongoose.connection.db.dropDatabase(function () {
+            mongoose.connection.close(function () {
+                done();
+            });
+        });
+    });
+
+    it("should find user by id", async () => {
+        
+        let expectedResult = "AlTimofeevM"
+        let result =  await db.findUserById("5e55785f222236202364b390");
+        if(result.token!==expectedResult){
+            throw new Error(`Expected ${expectedResult}, but got ${result}`)
+        }
+    })
+
+    it("should find user by token", async () => {
+        
+        let expectedResult = "AlTimofeevM"
+        let result =  await db.findUserByToken("AlTimofeevM");
+        if(result.token!==expectedResult){
+            throw new Error(`Expected ${expectedResult}, but got ${result}`)
+        }
+    })
+
+    it("should find user by email", async () => {
+        
+        let expectedResult = "AlTimofeevM"
+        let result =  await db.findUserByEmail("al.timofeev.m@yandex.ru")
+        if(result.token!==expectedResult){
+            throw new Error(`Expected ${expectedResult}, but got ${result}`)
+        }
+    })
+
+    it("should find question by id", async () => {
+        
+        let expectedResult = "Найти все возможные события"
+        let result =  await db.findQuestionById("5e557905222236202364b391")
+        if(result.title!==expectedResult){
+            throw new Error(`Expected ${expectedResult}, but got ${result}`)
+        }
+    })
+
+    it("should find answer by id", async () => {
+        
+        let expectedResult = "5e557905222236202364b391"
+        let result =  await db.findAnswerById("5e557b6d222236202364b392")
+        if(result.question!==expectedResult){
+            throw new Error(`Expected ${expectedResult}, but got ${result}`)
+        }
+    })
+
+
 })
-
-it("should find user by token", async () => {
-     
-    let expectedResult = "AlTimofeevM"
-    let result =  await db.findUserByToken("AlTimofeevM");
-    if(result.token!==expectedResult){
-        throw new Error(`Expected ${expectedResult}, but got ${result}`)
-    }
-})
-
-it("should find user by email", async () => {
-     
-    let expectedResult = "AlTimofeevM"
-    let result =  await db.findUserByEmail("al.timofeev.m@yandex.ru")
-    if(result.token!==expectedResult){
-        throw new Error(`Expected ${expectedResult}, but got ${result}`)
-    }
-})
-
-it("should find question by id", async () => {
-     
-    let expectedResult = "Найти все возможные события"
-    let result =  await db.findQuestionById("5e557905222236202364b391")
-    if(result.title!==expectedResult){
-        throw new Error(`Expected ${expectedResult}, but got ${result}`)
-    }
-})
-
-it("should find answer by id", async () => {
-     
-    let expectedResult = "5e557905222236202364b391"
-    let result =  await db.findAnswerById("5e557b6d222236202364b392")
-    if(result.question!==expectedResult){
-        throw new Error(`Expected ${expectedResult}, but got ${result}`)
-    }
-})
-
